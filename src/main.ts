153cb506fa4b4de43bd4dd604337f987c02b89e7
peiyuanxin1240314976 <18324545205@163.com>
2026-04-02 10:29:09 +0800

    perf: 登录页面UI更改

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,8 @@ import '@styles/index.scss'                         // 样式
 import '@utils/sys/console.ts'                      // 控制台输出内容
 import { setupGlobDirectives } from './directives'
 import { setupErrorHandle } from './utils/sys/error-handle'
+import VueParticles from '@tsparticles/vue3'
+import { loadSlim } from '@tsparticles/slim'
 
 document.addEventListener(
   'touchstart',
@@ -22,4 +24,9 @@ setupGlobDirectives(app)
 setupErrorHandle(app)
 
 app.use(language)
+app.use(VueParticles, {
+  init: async (engine) => {
+    await loadSlim(engine)
+  }
+})
 app.mount('#app')
